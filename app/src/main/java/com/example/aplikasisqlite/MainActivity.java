d5dfdc6b387d5cdecc772eae9778b766c450ba1d
Ranu <adamrain01@gmail.com>
2021-08-15 23:03:27 +0700

    Commit Aplikasi SQLite

---
+++ app/src/main/java/com/example/aplikasisqlite/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.aplikasisqlite;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.aplikasisqlite.helper.DbHelper;
+import com.example.aplikasisqlite.model.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    AlertDialog.Builder dialog;
+    List<Data> itemlist = new ArrayList<Data>();
+    Adapter adapter;
+    DbHelper SQLite = new DbHelper(this);
+
+    public static final String TAG_ID       = "id";
+    public static final String TAG_NAME     = "name";
+    public static final String TAG_ADDRESS  = "address";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Tambah SQLite
+        SQLite = new DbHelper(getApplicationContext());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        //Tambah Listview
+        listView    = (ListView) findViewById(R.id.listView);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                //Tambah Intent Untuk pindah ke halaman Add dan Edit
+                Intent intent = new Intent(MainActivity.this, AddEdit.class);
+                startActivity(intent);
+            }
+        });
+        //Tambah adapter dan listview
+        adapter = new Adapter(MainActivity.this, itemlist);
+        listView.setAdapter(adapter);
+
+        //Tekan lama daftar lisview untuk mendampilkan edit dan hapus
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final String idx    = itemlist.get(position).getId();
+                final String name   = itemlist.get(position).getName();
+                final String address = itemlist.get(position).getAddress();
+
+                final CharSequence[] dialogitem = {"Edit", "Delete"};
+                dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setCancelable(true);
+                dialog.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case 0:
+                                Intent intent = new Intent(MainActivity.this, AddEdit.class);
+                                intent.putExtra(TAG_ID, idx);
+                                intent.putExtra(TAG_NAME, name);
+                                intent.putExtra(TAG_ADDRESS, address);
+                                startActivity(intent);
+                                break;
+
+                            case 1:
+                                SQLite.delete(Integer.parseInt(idx));
+                                itemlist.clear();
+                                getAllData();
+                                break;
+                        }
+                    }
+                }).show();
+                return false;
+            }
+        });
+        getAllData();
+    }
+
+    private void getAllData(){
+        ArrayList<HashMap<String, String>> row = SQLite.getAllData();
+
+        for (int i=0; i<row.size(); i++){
+            String id       = row.get(i).get(TAG_ID);
+            String poster   = row.get(i).get(TAG_NAME);
+            String title    = row.get(i).get(TAG_ADDRESS);
+
+            Data data = new Data();
+
+            data.setId(id);
+            data.setName(poster);
+            data.setAddress(title);
+            itemlist.add(data);
+        }
+        adapter.notifyAll();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        itemlist.clear();
+        getAllData();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
